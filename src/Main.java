ddf4f18c7213e046d4df07ab896d9d1fbb472496
dmanier <damienmanier12@gmail.com>
2015-11-03 08:14:42 -0500

    Committed Aaron's main class with comments.  The patch claimed conflict so this was the easiest way to insert it.

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,26 @@
-import javax.swing.SwingUtilities;
-
 /**
  *
  * @author Aaron
  */
+
+// Imports used by Main
+import javax.swing.SwingUtilities;
+
+// Main class for the program to run off of
 public class Main implements Runnable {
-    
+
+    // Main method for running program
     public static void main(String[] args) {
+        /** Makes use of SwingUtilities to invoke the main method after all
+         *  of the other methods have been run in the program.
+         */
         SwingUtilities.invokeLater(new Main());
     }
-    
+
     @Override
+    // Method to run the thread with TopFrame
     public void run() {
         new TopFrame();
     }
+
 }
